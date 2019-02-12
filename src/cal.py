0ecc1df0ba802bc1d0f8c23998923beff97eacf1
SavannahGreuel <smallidgesavannah@gmail.com>
2019-02-12 14:40:00 -0700

    cal.py complete

--- src/cal.py
+++ src/cal.py
@@ -6,11 +6,11 @@ https://docs.python.org/3.6/library/calendar.html
 Write a program that accepts user input of the form
   `calendar.py month [year]`
 and does the following:
- - If the user doesn't specify any input, your program should 
-   print the calendar for the current month. The 'datetime'
-   module may be helpful for this.
- - If the user specifies one argument, assume they passed in a
-   month and render the calendar for that month of the current year.
+#  - If the user doesn't specify any input, your program should 
+#    print the calendar for the current month. The 'datetime'
+#    module may be helpful for this.
+#  - If the user specifies one argument, assume they passed in a
+#    month and render the calendar for that month of the current year.
  - If the user specifies two arguments, assume they passed in
    both the month and the year. Render the calendar for that 
    month and year.
@@ -22,3 +22,15 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+if len(sys.argv) == 1:
+  calendar.TextCalendar(0).prmonth(datetime.now().year, datetime.now().month)
+
+elif len(sys.argv) == 2:
+  calendar.TextCalendar(0).prmonth(datetime.now().year, int(sys.argv[1]))
+
+elif len(sys.argv) == 3:
+  calendar.TextCalendar(0).prmonth(int(sys.argv[2]), int(sys.argv[1]))
+
+else:
+  print('''This program is expecting two integer arguments to be passed in in the from of "calendar.py month [year]" ''' )
